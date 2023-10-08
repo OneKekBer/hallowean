e3c2a37035ad2d3a1970693db33190db24d168b3
OneKek <iliaberezovsky227@gmai.com>
2023-10-08 16:43:40 +0200

    6

--- src/components/Programa.jsx
+++ src/components/Programa.jsx
@@ -2,14 +2,17 @@ import pumpkin from "../assets/pumpkin.png";
 import bat from "../assets/bat.png";
 import cotel from "../assets/cotel.png";
 import Button from "./../common/Button";
-
+import { useRef } from "react";
 import swiper1 from "./../assets/swiper/1.png";
 import swiper2 from "./../assets/swiper/2.png";
 import swiper3 from "./../assets/swiper/3.png";
 import swiper4 from "./../assets/swiper/4.png";
 
+import scroll from "./../assets/scroll.png";
+import orange_scroll from "./../assets/orange_scroll.png";
+
 // import Swiper core and required modules
-import { Pagination } from "swiper/modules";
+import { Pagination, Navigation } from "swiper/modules";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -20,6 +23,8 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 export const Programa = () => {
+   const swiperRef = useRef();
+
    return (
       <div className="program bg-violet">
          <div className="wrapper">
@@ -104,12 +109,15 @@ export const Programa = () => {
             <div>
                <Swiper
                   // install Swiper modules
-                  modules={[Pagination]}
+                  modules={[Pagination, Navigation]}
                   spaceBetween={50}
                   loop={true}
                   slidesPerView={1}
                   pagination={{ clickable: true }}
-                  className="swiper h-[60vh] mt-10 rounded-lg "
+                  className="swiper h-[60vh] relative mt-10 rounded-lg "
+                  onBeforeInit={(swiper) => {
+                     swiperRef.current = swiper;
+                  }}
                >
                   <SwiperSlide>
                      <img
@@ -139,8 +147,21 @@ export const Programa = () => {
                         alt=""
                      />
                   </SwiperSlide>
-                  ...
+                  <div
+                     className="absolute md:block hidden top-1/2 transform -translate-y-1/2 z-10 right-10"
+                     onClick={() => swiperRef.current?.slideNext()}
+                  >
+                     <img src={scroll} className="w-[90px] h-[90px]" alt="" />
+                  </div>
                </Swiper>
+               <div className="w-full flex md:hidden justify-end">
+                  <img
+                     src={orange_scroll}
+                     className="w-[50px] h-[50px]"
+                     onClick={() => swiperRef.current?.slideNext()}
+                     alt=""
+                  />
+               </div>
             </div>
          </div>
       </div>
